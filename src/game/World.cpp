39f7dfa3e935ddecb10afb7820c9f4ce34543379
Redcore <redcorex@gmail.com>
2009-11-11 14:49:55 +0300

    Revert [patch 130] Auction Bot (useless for a while)

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -63,7 +63,6 @@
 #include "WaypointManager.h"
 #include "GMTicketMgr.h"
 #include "Util.h"
-#include "AuctionHouseBot.h"
 
 INSTANTIATE_SINGLETON_1( World );
 
@@ -1540,8 +1539,6 @@ void World::SetInitialWorldSettings()
     uint32 nextGameEvent = sGameEventMgr.Initialize();
     m_timers[WUPDATE_EVENTS].SetInterval(nextGameEvent);    //depend on next event
     sLog.outString("Starting Autobroadcast system by Xeross..." );
-    sLog.outString("Initialize AuctionHouseBot...");
-    auctionbot.Initialize();
     sLog.outString( "WORLD: World initialized" );
 
     uint32 uStartInterval = getMSTimeDiff(uStartTime, getMSTime());
@@ -1614,7 +1611,6 @@ void World::Update(uint32 diff)
     /// <ul><li> Handle auctions when the timer has passed
     if (m_timers[WUPDATE_AUCTIONS].Passed())
     {
-        auctionbot.Update();
         m_timers[WUPDATE_AUCTIONS].Reset();
 
         ///- Update mails (return old mails with item, or delete them)
