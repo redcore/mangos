0e171a223fded4461587b914bc0546f5e90d82b1
de-dima <primarh58@gmail.com>
2009-12-02 12:46:10 +0300

    Update SD2 (1513)

--- src/bindings/ScriptDev2/scripts/northrend/violet_hold/violet_hold.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/violet_hold/violet_hold.cpp
@@ -281,7 +281,7 @@ bool GossipHello_npc_sinclari(Player* pPlayer, Creature* pCreature)
         pPlayer->PrepareQuestMenu( pCreature->GetGUID() );
 
     pPlayer->ADD_GOSSIP_ITEM( 0, "I am ready.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
-    pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetGUID());
 
     return true;
 }
